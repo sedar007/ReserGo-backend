e3703cb41423d2dc954289380af350e68d0c7063
sedar <desiradjysedar@gmail.com>
2025-03-23 15:04:40 +0100

    change config Cloudinary

--- ReserGo.Tiers/Models/CloudinaryModel.cs
+++ ReserGo.Tiers/Models/CloudinaryModel.cs
@@ -12,15 +12,17 @@ public class CloudinaryModel {
     {
         _logger = logger;
         _logger.LogInformation("Initializing CloudinaryModel");
-
-        var cloudinarySettings = configuration.GetSection("Cloudinary").Get<AppSettingsCloudinary>();
-        if (cloudinarySettings == null)
+        string? cloudinaryCloudName = configuration.GetSection("CloudinaryCloudName")?.Get<string>() ;
+        string? cloudinaryApiKey = configuration.GetSection("CloudinaryApiKey")?.Get<string>();
+        string? cloudinaryApiSecret = configuration.GetSection("CloudinaryApiSecret")?.Get<string>();
+        if(cloudinaryCloudName == null || cloudinaryApiKey == null || cloudinaryApiSecret == null)
         {
             _logger.LogError("Cloudinary settings are not configured properly.");
-            throw new ArgumentNullException(nameof(cloudinarySettings), "Cloudinary settings are missing.");
+            throw new ArgumentNullException(nameof(cloudinaryCloudName), "Cloudinary settings are missing.");
         }
+       
 
-        var account = new Account(cloudinarySettings.CloudName, cloudinarySettings.ApiKey, cloudinarySettings.ApiSecret);
+        var account = new Account(cloudinaryCloudName, cloudinaryApiKey, cloudinaryApiSecret);
         _cloudinary = new Cloudinary(account);
 
         _logger.LogInformation("CloudinaryModel initialized successfully");
@@ -34,7 +36,7 @@ public class CloudinaryModel {
 }
 
 public class AppSettingsCloudinary {
-    public string CloudName { get; init; } = null!;
-    public string ApiKey { get; init; } = null!;
-    public string ApiSecret { get; init; } = null!;
+    public string CloudinaryCloudName { get; init; } = null!;
+    public string CloudinaryApiKey { get; init; } = null!;
+    public string CloudinaryApiSecret { get; init; } = null!;
 }
