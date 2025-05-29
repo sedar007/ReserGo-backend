544d2342de95dd771180f4ee0df96c3cead4d734
laurencia06 <ranlauren633@gmail.com>
2025-05-29 23:11:13 +0200

    update database

---
+++ ReserGo.DataAccess/Migrations/20250529183244_AddBookingsToRoomAvailabilit.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReserGo.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBookingsToRoomAvailabilit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "RoomAvailabilityId",
+                table: "BookingHotel",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookingHotel_RoomAvailabilityId",
+                table: "BookingHotel",
+                column: "RoomAvailabilityId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BookingHotel_RoomAvailability_RoomAvailabilityId",
+                table: "BookingHotel",
+                column: "RoomAvailabilityId",
+                principalTable: "RoomAvailability",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BookingHotel_RoomAvailability_RoomAvailabilityId",
+                table: "BookingHotel");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BookingHotel_RoomAvailabilityId",
+                table: "BookingHotel");
+
+            migrationBuilder.DropColumn(
+                name: "RoomAvailabilityId",
+                table: "BookingHotel");
+        }
+    }
+}
