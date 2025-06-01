bda27935ab6d774095b97b56f9c42810ac3fde12
laurencia06 <ranlauren633@gmail.com>
2025-06-01 14:45:02 +0200

    rm nb Room

--- ReserGo.Common/Requests/Products/Hotel/HotelSearchAvailabilityRequest.cs
+++ ReserGo.Common/Requests/Products/Hotel/HotelSearchAvailabilityRequest.cs
@@ -2,7 +2,6 @@ namespace ReserGo.Common.Requests.Products.Hotel;
 
 public class HotelSearchAvailabilityRequest {
     public int NumberOfPeople { get; init; } 
-    public int NumberOfRooms { get; init; }
     public DateOnly ArrivalDate { get; init; }
     public DateOnly ReturnDate { get; init; }
 }
